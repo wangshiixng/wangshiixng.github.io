1b7a2ad5ef7db02f9e883fe7b7efcaba732bc070
wangshiixng <979291241@qq.com>
2016-10-19 16:55:45 +0800

    添加内容

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -1,1 +1,29 @@
 var app =angular.module('app',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+  $routeProvider
+  .when('/',{
+     templateUrl:'./assets/tpl/index.html',
+     controller:''
+
+  })
+.when('/blog',{
+  templateUrl:'./assets/tpl/blog.html',
+  controller:''
+
+})
+.when('/book',{
+  templateUrl:'./assets/tpl/book.html',
+  controller:'bookController'
+
+})
+.when({
+          templateUrl:'./assets/tpl/about_me.html',
+          controller:''
+        })
+
+  otherwise({
+  redirectTo:'/'
+
+  })
+})]
---
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -0,0 +1,8 @@
+app.controller('bookController',['$scope',function($scope){
+$scope.bookTypes=[
+  {id:'ertong',name:'儿童',img:'',description:''},
+  {id:'lishi',name:'儿童',img:'',description:''},
+  {id:'dongman',name:'儿童',img:'',description:''},
+  {id:'qingchunwenxue',name:'儿童',img:'',description:''},  
+]
+}])
