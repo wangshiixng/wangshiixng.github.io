0db6a282edd0aa2613317f21b7064c17adb717c0
wangshiixng <979291241@qq.com>
2016-10-19 15:20:39 +0800

    初始化整个框架

---
+++ dangdang-app-angularjs/assets/api/service.js
@@ -0,0 +1,24 @@
+app.factory('commonService',['$http',function($http){
+var service={};
+/**
+ * 根据分类信息取数据
+ * @param  {[type]} type     [description]
+ * @param  {[type]} callBack [description]
+ * @return {[type]}          [description]
+ */
+service.getData=function(type,callBack){
+ $http({
+ url:'../../data/book_'+type+'.json',
+ method:'get'
+
+ })
+ .then(function(res){
+   console.log('获取数据成功');
+ callBack(res);
+
+ },function(err){
+
+    console.dir(err);
+ })
+}
+}])
---
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -0,0 +1,8 @@
+app.controller('mainController',['$scope','commonService',function{($scope, c_service){
+
+ c_service.getData('ertong',function(res){
+
+    console.dir(res);
+ })
+
+}])
