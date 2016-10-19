bd2c12be469cba542bca743f08b9ee5c2137b8ea
wangshiixng <979291241@qq.com>
2016-10-19 15:45:15 +0800

    修改代码

--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,4 +1,4 @@
-app.controller('mainController', ['$scope', 'commonService',function($scope, c_service) {
+app.controller('mainController', ['$scope', 'commonService', function($scope, c_service) {
     c_service.getData('ertong', function(res) {
         console.dir(res);
     });
