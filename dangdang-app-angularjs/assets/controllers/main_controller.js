46661f55b791951974c0bea5f1751c3e099e72e6
wangshiixng <979291241@qq.com>
2016-10-19 16:57:42 +0800

    写数据

--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,7 +1,7 @@
 app.controller('mainController', ['$scope', 'commonService', function($scope, c_service) {
-    // c_service.getData('ertong', function(res) {
-    //     console.dir(res);
-    // });
+    c_service.getData('ertong', function(res) {
+        console.dir(res);
+    });
 
     $scope.selectedIndex=0;
     $scope.linkTo=function(index){
