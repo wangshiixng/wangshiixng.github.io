a86c60f83feda5c3aa9c8f3c553a250cfd02cd93
wangshiixng <979291241@qq.com>
2016-10-19 17:25:27 +0800

    添加数据

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -21,6 +21,12 @@ app.config(['$routeProvider',function($routeProvider){
       templateUrl:'./assets/tpl/about_me.html',
       controller:''
       })
+.when('/book_list',{
+   templateUrl:'./assets/tpl/book_list.html',
+   controller:'bookListController'
+
+
+})
 
   otherwise({
   redirectTo:'/'
--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,9 @@
+
 app.controller('bookController',['$scope',function($scope){
 $scope.bookTypes=[
-  {id:'ertong',name:'儿童',img:'',description:''},
-  {id:'lishi',name:'儿童',img:'',description:''},
-  {id:'dongman',name:'儿童',img:'',description:''},
-  {id:'qingchunwenxue',name:'儿童',img:'',description:''},  
+  {id:'ertong',name:'儿童',img:'',description:'最近7天热卖的动漫书籍'},
+  {id:'lishi',name:'儿童',img:'',description:'最近7天热卖的历史书籍'},
+  {id:'dongman',name:'儿童',img:'',description:'最近7天热卖的儿童书籍'},
+  {id:'qingchunwenxue',name:'儿童',img:'',description:'最近7天热卖的青春文学书籍'},
 ]
 }])
