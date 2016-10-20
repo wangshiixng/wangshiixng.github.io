8277f4b7835b355e67cb5279fbd3b771c9049b1a
wangshiixng <979291241@qq.com>
2016-10-20 09:49:20 +0800

    修改数据

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,6 +1,6 @@
 app.controller('bookListController',['$scope','$routeParams','commonService',function($scope,$routeParams,c_s){
 $scope.data=[];
-  c.s.getData($routeParams.id,function(res){
+  c_s.getData($routeParams.id,function(res){
     $scope.data=res.data;
   })
 }])
