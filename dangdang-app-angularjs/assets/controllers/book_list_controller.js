ee785deeb24f15acc03a4c80b02d9adca41a1458
wangshiixng <979291241@qq.com>
2016-10-20 09:45:21 +0800

    提交数据

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,6 +1,6 @@
 app.controller('bookListController',['$scope','$routeParams','commonService',function($scope,$routeParams,c_s){
 $scope.data=[];
   c.s.getData($routeParams.id,function(res){
-    $scope.data=res.data.data;
+    $scope.data=res.data;
   })
 }])
